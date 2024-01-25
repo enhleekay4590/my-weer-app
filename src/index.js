83654b70fde07079bf3af521e8561ba83ba1ff96
EnhleeKay <minenhle.kubheka241@gmail.com>
2024-01-26 00:29:41 +0200

    Edited HTML and CSS and also added functionality on my JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let weatherTempreture = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  let cityNameElement = document.querySelector("#city-name");
+
+  cityNameElement.innerHTML = response.data.city;
+  weatherTempreture.innerHTML = Math.round(temperature);
+}
+
+function searchForCity(city) {
+  let apiKey = "003ed39ao8f2b2339d6b100400t60f78";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchElement(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityNameElement = document.querySelector("#city-name");
-  cityNameElement.innerHTML = searchInputElement.value;
+
+  searchForCity(searchInputElement.value);
 }
 
 let formElement = document.querySelector("#form-info");
 formElement.addEventListener("submit", searchElement);
+
+searchForCity("Cape Town");
