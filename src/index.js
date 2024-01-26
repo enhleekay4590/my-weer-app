b78fb4aa602873c1252d1892ab068b7e594754b1
EnhleeKay <minenhle.kubheka241@gmail.com>
2024-01-26 15:11:24 +0200

    Updated more data information and also updated HTML

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
   let weatherTempreture = document.querySelector("#weather-temperature");
   let temperature = response.data.temperature.current;
   let cityNameElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityNameElement.innerHTML = response.data.city;
   weatherTempreture.innerHTML = Math.round(temperature);
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchForCity(city) {
